530c99411aa78173ad7b43ac96becdf3c66f13dd
Drew Butler <abutler3@gmail.com>
2013-11-09 09:35:05 -0600

    Edit test and create root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Crumblr::Application.routes.draw do
   get "pages/about"
-  get "pages/welcome"
+
+  root 'pages#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
