d8b7d455d6b7238ec2742e500c930935a13aba7c
Drew Butler <abutler3@gmail.com>
2013-11-09 09:25:20 -0600

    Add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Crumblr::Application.routes.draw do
+  get "pages/about"
+  get "pages/welcome"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
