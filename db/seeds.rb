186b9a7da78553709bd4a7b30e2f1f4abd421b4c
Drew Butler <abutler3@gmail.com>
2013-11-16 14:57:25 -0600

    Add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#Cheesecake
+# post1 = Post.new
+# post1.title = ""
+# post1.kind = "image"
+# post1.content = ""
+# post1.save
+
+# 2.times do
+#   post1.hearts.create
+# end
+# #Ovenly
+# post2 = Post.new(title: "", kind: "text")
+# post2.content = ""
+# post2.save
+
+# 5.times { post2.hearts.create }
+
+# # Cake
+# post3 = Post.new(
+#   title: "",
+#   kind: "image",
+#   content: ""
+# )
+
+# 8.times { post3.hearts.create }
+
+# #Blondie
+# post4 = Post.new(title: "", kind: "text")
+# # This is something called a heredoc. Treats all content between content asa string
+# post4.content = <<CONTENT
+
+# CONTENT
+# post4.save
+
+# 6.times { post4.hearts.create }
+
+
+
+
